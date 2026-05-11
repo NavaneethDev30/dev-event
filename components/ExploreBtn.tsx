2d1a962e1da36b0baa3ed20a3b175d35d6e739a1
Navaneeth dev <navaneethdev33@gmail.com>
2026-05-11 15:12:01 +0530

    added files

---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Image from "next/image"
+const ExploreBtn = () => {
+  return (
+    <button type="button" id="explore-btn" className="mt-5 mx-auto text-center" onClick={()=>{console.log("button clicked")}}>
+      
+      <a href="#events">
+      Explore Events
+      <Image src='/icons/arrow-down.svg' width={20} height={20} alt="explore-img"></Image>
+      </a>
+      </button>
+      
+  )
+}
+
+export default ExploreBtn
